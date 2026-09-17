e4013b7c32d46998886647ba8eb1d71fb3406d90
Noir <raihanalirachman031107@gmail.com>
2026-09-17 09:31:43 +0700

    about done, now services

--- Minggu 1/corporate/src/components/Header.js
+++ Minggu 1/corporate/src/components/Header.js
@@ -6,7 +6,7 @@ export default function Header() {
     return(
         <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home">Corporate</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
--- Minggu 1/corporate/src/components/about.js
+++ Minggu 1/corporate/src/components/about.js
@@ -2,11 +2,17 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image';
+import ProgressBar from 'react-bootstrap/ProgressBar';
+
 
 import img1 from '../assets/images/img1.jpg';
 
 
 export default function AppAbout() {
+    const html = 80;
+    const responsive = 95;
+    const photoshop = 50;
+
     return(
         <section id="about" className="block about-block">
            <Container fluid>
@@ -19,7 +25,20 @@ export default function AppAbout() {
                 <Image src={img1}/>
                 </Col>
                 <Col sm={6}>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                <p>Welcome to Corporate, where professionalism meets innovation. We are dedicated to helping businesses and individuals achieve their goals with tailored solutions that inspire growth and success. Our team combines expertise, creativity, and technology to deliver services that truly make a difference. At Corporate, we value trust, integrity, and long-term partnerships. We believe every client deserves personalized attention and results that exceed expectations. Whether you’re looking to strengthen your brand, streamline operations, or plan for the future, we are here to guide you every step of the way. Corporate is more than a name—it’s your partner in progress.</p>
+                
+                <div className="progress-block">
+                    <h4>HTML / CSS / Javascript</h4>
+                    <ProgressBar now={html} label={`${html}%`} />
+                </div>
+                <div className="progress-block">
+                    <h4>RESPONSIVE</h4>
+                    <ProgressBar now={responsive} label={`${responsive}%`} />
+                </div>
+                <div className="progress-block">
+                    <h4>PHOTOSHOP</h4>
+                    <ProgressBar now={photoshop} label={`${photoshop}%`} />
+                </div>
                 </Col>
             </Row>
             </Container>
