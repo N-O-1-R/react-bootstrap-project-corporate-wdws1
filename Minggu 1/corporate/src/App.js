080b8a47450960164bff1f995a6575cda5982021
Noir <raihanalirachman031107@gmail.com>
2026-09-17 08:20:12 +0700

    workshop desain web ini, terakhir video ke 5

---
+++ Minggu 1/corporate/src/App.js
@@ -0,0 +1,20 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+import AppHeader from './components/Header';
+import AppHero from './components/Hero';
+
+function App() {
+  return (
+    <div className="App">
+      <header id="header">
+        <AppHeader />
+      </header> 
+      <main>
+        <AppHero />
+      </main>
+    </div>
+  );
+}
+
+export default App;
